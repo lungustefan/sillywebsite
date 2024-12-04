e3f1ac18547bc059629ce058f9d305b01ae13fd4
Lungu Stefan-Gabriel <hi@lungustefan.ro>
2024-12-04 15:30:42 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/xpugno-logo.svg"
+          src="/fc9f231555fc961fad5e303d2610c952.webp"
           alt="Stefan logo"
           width={180}
           height={38}
